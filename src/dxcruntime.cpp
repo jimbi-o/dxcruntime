f54241eb92d7500d9f203f3ec8413014b8956ca8
jimbi-o <35837796+jimbi-o@users.noreply.github.com>
2019-10-21 08:15:21 +0900

    dxc initialized

--- src/dxcruntime.cpp
+++ src/dxcruntime.cpp
@@ -1,6 +1,8 @@
-#include <dxgi1_6.h>
+#include <dxgi1_6.h> // must include before dxcapi
 #include "dxc/Support/dxcapi.use.h"
 #include "doctest/doctest.h"
-TEST_CASE("doctest test") {
-  CHECK(true == true);
+TEST_CASE("dxc test") {
+  dxc::DxcDllSupport dxcdllsupport;
+  auto dllresult = dxcdllsupport.Initialize();
+  CHECK(dllresult == 0);
 }
